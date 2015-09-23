ceab93d781f71a6804283e3f3d069c2082e72e75
khanhchi94 <lekhanhchi1994@gmail.com>
2015-09-23 09:19:07 +0700

    Update TestPT.java

--- TestPT.java
+++ TestPT.java
@@ -1,11 +1,30 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestPT {
 
+	GiaiPT giaiPT ;
+
+	@BeforeClass
+	public static void onTimeSetUp() {
+	// one-time initialization code 
+		System.out.println("@BeforeClass - oneTimeSetUp");	
+	}
+	@AfterClass
+	public static void onTimeTearDown(){
+	// one-time cleanup code
+		System.out.println("@AfterClass - oneTimeTearDown");
+	}
+	@Before
+	public void setUp() throws Exception {
+		giaiPT = new GiaiPT();
+	}
+	@After
+	public void tearDown() throws Exception{}
+	
 	@Test
-	public void test() {
+	public void testPT1() {
+	
 		fail("Not yet implemented");
 	}
 
